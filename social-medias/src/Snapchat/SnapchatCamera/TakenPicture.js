d0f65ffe9bcec993dd623cceccbbeee7a0873c85
rachid <rachid.lhasnaoui@epitech.eu>
2022-12-29 01:37:10 +0100

    Save the picture we just took on our device

--- social-medias/src/Snapchat/SnapchatCamera/TakenPicture.js
+++ social-medias/src/Snapchat/SnapchatCamera/TakenPicture.js
@@ -1,7 +1,19 @@
+import React, { useState, useEffect } from "react";
 import { Text, TouchableOpacity, View, ImageBackground } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+
+import * as MediaLibrary from "expo-media-library";
 
 export default function TakenPicture(props) {
+  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
+  const save = (imageUri) => {
+    MediaLibrary.saveToLibraryAsync(imageUri);
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
   return (
     <View style={{ height: "100%", width: "100%" }}>
       <ImageBackground source={{ uri: props.imageUri }} style={{ flex: 1 }}>
@@ -10,6 +22,12 @@ export default function TakenPicture(props) {
           onPress={() => props.onChange(null)}
         >
           <Ionicons name="close-outline" color={"white"} size={42} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ top: "80%" }}
+          onPress={() => save(props.imageUri)}
+        >
+          <MaterialIcons name="save-alt" color={"white"} size={30} />
         </TouchableOpacity>
       </ImageBackground>
     </View>
